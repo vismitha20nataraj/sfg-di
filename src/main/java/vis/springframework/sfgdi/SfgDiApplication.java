ebf41a63dd5de45e56ea03462dddc8b00d9ae86e
vismitha20nataraj <vismitha20nataraj@gmail.com>
2021-04-15 15:55:23 +0200

    Add test and sfg-di app files

---
+++ src/main/java/vis/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,39 @@
+package vis.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import vis.springframework.sfgdi.controller.ConstructorInjectedController;
+import vis.springframework.sfgdi.controller.MyController;
+import vis.springframework.sfgdi.controller.PropertyInjectedController;
+import vis.springframework.sfgdi.controller.SetterInjectedController;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
+
+	}
+
+}
